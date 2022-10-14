dfd4f760812db792448b369a03d57b2ce47493d0
Yuki Onodera <(maroonv4869@icloud.com)>
2022-10-14 20:56:03 +0900

    fix allowedhost in settings

--- server/config/settings.py
+++ server/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = list(os.getenv('ALLOWED_HOSTS'))
 
 AUTH_USER_MODEL = "users.User"
 
