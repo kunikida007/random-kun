e4af0ff4ea1250b6089bb3073eda9c392f27becb
Yuki Onodera <(maroonv4869@icloud.com)>
2022-10-18 23:38:59 +0900

    fix static_root

--- server/config/settings.py
+++ server/config/settings.py
@@ -125,16 +125,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
-print(STATICFILES_DIRS)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
